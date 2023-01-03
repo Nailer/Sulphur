1a87aa15f579ddcad3b3b089f83d84123b599e27
Emmanuel Aje <53949905+Nailer@users.noreply.github.com>
2023-01-03 22:31:52 +0100

    finished project

--- hack.js
+++ hack.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar")
+var contents = document.querySelector(".container")
 
 menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar")
+    contents.classList.toggle("checker")
 }
