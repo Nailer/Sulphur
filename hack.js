adcab8cd48d2fff7b6f6a1aa06f363136c83621b
Sukuna <ajeemmanuel221@gmail.com>
2024-10-20 21:56:21 +0100

    added the livepeer page

--- hack.js
+++ hack.js
@@ -2,42 +2,42 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar")
 // import { Livepeer } from "livepeer";
 var contents = document.querySelector(".container")
-var tus = require('tus-js-client')
-var player = videojs('livepeer-sample-videojs-player');
-player.play();
+// var tus = require('tus-js-client')
+// var player = videojs('livepeer-sample-videojs-player');
+// player.play();
 
 menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar")
     contents.classList.toggle("checker")
 }
 
-const input = document.getElementById('fileInput');
+// const input = document.getElementById('fileInput');
 
-const file = input.files[0];
+// const file = input.files[0];
 
-const upload = new tus.Upload(file, {
-  endpoint: tusEndpoint,
-  metadata: {
-    filename,
-    filetype: 'video/mp4',
-  },
-  uploadSize: file.size,
-  onError(err) {
-    console.error('Error uploading file:', err);
-  },
-  onProgress(bytesUploaded, bytesTotal) {
-    const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
-    console.log('Uploaded ' + percentage + '%');
-  },
-  onSuccess() {
-    console.log('Upload finished:', upload.url);
-  },
-});
+// const upload = new tus.Upload(file, {
+//   endpoint: tusEndpoint,
+//   metadata: {
+//     filename,
+//     filetype: 'video/mp4',
+//   },
+//   uploadSize: file.size,
+//   onError(err) {
+//     console.error('Error uploading file:', err);
+//   },
+//   onProgress(bytesUploaded, bytesTotal) {
+//     const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+//     console.log('Uploaded ' + percentage + '%');
+//   },
+//   onSuccess() {
+//     console.log('Upload finished:', upload.url);
+//   },
+// });
 
-const previousUploads = await upload.findPreviousUploads();
+// const previousUploads = await upload.findPreviousUploads();
 
-if (previousUploads.length > 0) {
-  upload.resumeFromPreviousUpload(previousUploads[0]);
-}
+// if (previousUploads.length > 0) {
+//   upload.resumeFromPreviousUpload(previousUploads[0]);
+// }
 
-upload.start();
+// upload.start();
